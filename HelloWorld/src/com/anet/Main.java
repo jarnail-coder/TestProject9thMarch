ae0b24dc3df6b7915fe4a301d456050e4b0536e4
Jarnail Singh <jarnailsingh45@gmail.com>
2017-03-09 16:26:40 +0530

    Function 2 commited

--- HelloWorld/src/com/anet/Main.java
+++ HelloWorld/src/com/anet/Main.java
@@ -2,7 +2,12 @@ package com.anet;
 
 public class Main {
 	public static void main(String[] args) {
-		//Jarnail Singh
+		// Jarnail Singh
 		System.out.println("THis is Jarnail");
+		function2();
+	}
+
+	public static void function2() {
+		System.out.println("HTis is funcation 2");
 	}
 }
