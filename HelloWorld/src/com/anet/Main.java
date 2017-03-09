b4b753d7cd3e795107cdba59b8941309227d8419
Jarnail Singh <jarnailsingh45@gmail.com>
2017-03-09 16:33:06 +0530

    function 1 added

--- HelloWorld/src/com/anet/Main.java
+++ HelloWorld/src/com/anet/Main.java
@@ -4,9 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		//Jarnail Singh
 		System.out.println("THis is Jarnail");
+		fun1();
 	}
 	public static void fun1()
 	{
 		//Function is incomplete
+		System.out.println("this is function 1");
 	}
 }
