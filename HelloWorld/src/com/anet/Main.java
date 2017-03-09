cd8d2347954e9097490f771710678e211d25a1b9
Jarnail Singh <jarnailsingh45@gmail.com>
2017-03-09 16:28:02 +0530

    Function 3 Commited

--- HelloWorld/src/com/anet/Main.java
+++ HelloWorld/src/com/anet/Main.java
@@ -2,7 +2,12 @@ package com.anet;
 
 public class Main {
 	public static void main(String[] args) {
-		//Jarnail Singh
-		System.out.println("THis is Jarnail");
+		// Jarnail Singh
+		System.out.println("This is Aman");
+		function3();
+	}
+
+	static void function3() {
+		System.out.println("Hti sis funcaiton 3");
 	}
 }
